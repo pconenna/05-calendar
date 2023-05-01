cc9ccaa95993beb8e47ae60960695f3c89094835
Peter Conenna <pconenna@depaul.edu>
2023-05-01 15:19:33 -0500

    function to save timeblock id and input to local storage

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  var saveBtn = $('.saveBtn');
+  saveBtn.on('click',function(){
+    //need id of the div that says hour-x
+    //need the content of the text area with class description
+    var time = $(this).parent();
+    var description =$(this).siblings('.description');
+    console.log(time.attr('id'))
+    localStorage.setItem(time.attr('id'),description.val())
+
+  })
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
